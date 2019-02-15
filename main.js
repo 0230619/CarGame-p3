2062d9d4c6cf9faa15dc7537fb8a1153377acc6c
Rodriguez <0230619@students.dadeschools.net>
2019-02-15 10:23:04 -0500

    Finished main game

--- main.js
+++ main.js
@@ -8,23 +8,65 @@ let car = {
 
     driveToWork(){
         alert(`Old Mileage: ${this.mileage}`);
-        this.mileage = this.mileage + 8;
+        this.mileage += 10;
         alert(`New Mileage: ${this.mileage}`);
     },
+   
+    driveAroundUSA(){
+        alert(`Old Mileage: ${this.mileage}`);
+        this.mileage += 6000;
+        alert(`New Mileage: ${this.mileage}`);
+        alert(`Car needs a tuneup!`);
+        this.isWorking = false;
+    },
+
+    getTuneUp(){
+        alert(`Car is fixed and ready to go!`);
+        this.isWorking = true;
+    },
+
+    honk(){
+        alert(`Honk! Honk!`);
+    }
 }
 
 
-// Functions
+// FUNCTIONS
+// Logs all of our car's current stats to the console
+const rewriteStats = () => {
+    console.log(`Make: ${car.make}`);
+    console.log(`Model: ${car.model}`);
+    console.log(`Color: ${car.color}`);
+    console.log(`Mileage: ${car.mileage}`);
+    console.log(`Is Working: ${car.isWorking}`);
+    console.log(`-------------------------------`);
+}
 
+
+// MAIN PROCESS
 /* Capture keyboard input. Depending on the letter pressed it
  will "call" (execute) different function. */
 document.onkeyup = (event) => {
 
     //Captures the key press, converts it to lowercase, and save it to a variable
     let letter = String.fromCharCode(event.keyCode).toLowerCase();
+    if(letter === `h`){
+        car.honk();
+        rewriteStats();
+    }
 
     if(letter === 'd'){
         car.driveToWork();
+        rewriteStats();
+    }
+
+    if(letter === 'r'){
+        car.driveAroundUSA();
+        rewriteStats();
+    }
+
+    if(letter === 't'){
+        car.getTuneUp();
         rewriteStats();
     }
 }
